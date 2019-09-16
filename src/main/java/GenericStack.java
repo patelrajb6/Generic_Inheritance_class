e31f3364723047c1216dfb53ab4e36978c344edd
Raj Patel <raj.patel@ymail.com>
2019-09-15 21:17:02 -0500

    GenericStack version 1

--- src/main/java/GenericStack.java
+++ src/main/java/GenericStack.java
@@ -1,4 +1,28 @@
 
-public class GenericStack extends GenericList{
+public class GenericStack<E> extends GenericList<E>{
+
+	@Override
+	public void add(E data) {
+		Node NewNode= new Node(data);
+		if(this.head==null)											//if the queue is empty
+		{
+			this.head=NewNode;
+		}
+		else
+		{
+			NewNode.next=this.head;
+			head=NewNode;
+		}
+		this.length=this.length+1;
+	}
+	public void push(E data)
+	{
+		this.add(data);
+	}
+	public E pop()
+	{
+		E PoppedVal= this.delete();
+		return PoppedVal;
+	}
 
 }
