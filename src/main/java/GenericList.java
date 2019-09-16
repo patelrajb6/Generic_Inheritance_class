285ef2e259c0f1f84dfd16c3e1b35e11e6fab4ab
Raj Patel <raj.patel@ymail.com>
2019-09-15 20:54:34 -0500

    Generic Queue version1

--- src/main/java/GenericList.java
+++ src/main/java/GenericList.java
@@ -1,7 +1,7 @@
 
 public abstract class GenericList<T> {				// the base singly Linked list 
 	Node head;										// reference to the first node in the list
-	int length;										// saving the length of the list	
+	int length=0;										// saving the length of the list	
 	
 	public abstract void add (T data);						// adding the node
 	public T delete()							//deleting the node
@@ -10,7 +10,7 @@ public abstract class GenericList<T> {				// the base singly Linked list
 		DeletedVal=this.head.data;				//storing the data of the first node before deleting
 		
 		this.head=this.head.next;				//moving the reference to the second node in the list
-		
+		this.length=this.length-1;
 		return DeletedVal;						
 	}
 	public void print()								// printing the elements of the list one element per line
@@ -23,7 +23,7 @@ public abstract class GenericList<T> {				// the base singly Linked list
 		}
 		
 	}
-	public class Node								// Node class which is the single unit of the list
+	public  class Node								// Node class which is the single unit of the list
 	{
 		T data;										// where data is stored
 		Node next;									// the address of the next element is stored.
--- src/main/java/GenericQueue.java
+++ src/main/java/GenericQueue.java
@@ -1,4 +1,37 @@
 
-public class GenericQueue extends GenericList {
 
+public class GenericQueue<I> extends GenericList<I> {				//class inherited from list class and also infers the type
+
+	GenericQueue(I data)											//constructor
+	{
+		this.add(data);
+	}
+	public void add(I data) {										//the implementation of the abstract method
+		Node NewNode=new Node(data);								//New Node	
+		Node temp= new Node(null);									// temp Node used as pointer
+		if(this.head==null)											//if the queue is empty
+		{
+			this.head=NewNode;
+		}
+		else
+		{
+			while(temp.next!=null)
+			{
+				temp=temp.next;
+			}
+			temp.next=NewNode;
+		}
+		this.length=length+1;
+	}
+	public void enqueue(I data)
+	{
+		this.add(data);
+	}
+	
+	public I dequeue ()
+	{
+		I deletedVal=this.delete();
+		return deletedVal;
+	}
+	
 }
